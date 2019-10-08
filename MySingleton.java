9faa876facfea87b802611a59805f9271867bc08
teppei.fushino <fushinoteppei1225@icloud.com>
2019-10-09 00:52:02 +0900

    10/8

---
+++ MySingleton.java
@@ -0,0 +1,13 @@
+public class MySingleton{
+    public static void main(String[] args){
+        private static final MySingleton instance = new MySingleton();
+        private MySingleton(){};
+        public static MySingleton getInstance(){
+            System.out.println("hello");
+            return instance;
+        }
+
+        getInstance();
+    }
+
+}
